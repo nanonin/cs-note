be5279cb95551fedf968dac4eb2e6786efd4d287
vvyun <1872385668@qq.com>
2020-08-23 17:32:27 +0800

    fix package

---
+++ design-pattern/src/com/vvyun/behavioral/chain/ChainPattern.java
@@ -0,0 +1,84 @@
+package com.vvyun.behavioral.chain;
+
+public class ChainPattern {
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG, "This is a debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR, "This is an error information.");
+    }
+
+    private static AbstractLogger getChainOfLoggers() {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+}
+
+abstract class AbstractLogger {
+    public static int INFO = 1;
+    public static int DEBUG = 1;
+    public static int ERROR = 1;
+
+    protected int level;
+
+    protected AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger abstractLogger) {
+        this.nextLogger = abstractLogger;
+    }
+
+    public void logMessage(int level, String message) {
+        if (this.level <= level) {
+            write(message);
+        }
+        if (nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+    }
+
+    abstract protected void write(String message);
+}
+
+class ConsoleLogger extends AbstractLogger {
+    public ConsoleLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
+
+class ErrorLogger extends AbstractLogger {
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
+
+class FileLogger extends AbstractLogger {
+    public FileLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
